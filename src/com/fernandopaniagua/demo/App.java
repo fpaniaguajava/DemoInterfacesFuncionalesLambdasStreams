255cf097f77fef0f964c87fdf7f509f26cea6efa
fpaniaguaangular <102974105+fpaniaguaangular@users.noreply.github.com>
2022-11-08 20:31:52 +0100

    Initial commit

---
+++ src/com/fernandopaniagua/demo/App.java
@@ -0,0 +1,21 @@
+package com.fernandopaniagua.demo;
+
+public class App {
+
+	public static void main(String[] args) {
+		java.util.ArrayList<Empleado> empleados;
+		empleados = GeneradorEmpleados.generarEmpleados(1000);
+		//¿Hay empleados que cobran más de 70000?
+		
+		//Listado de empleados que cobran más de 70000
+		
+		//Número de empleados que cobran más de 70000
+		
+		//¿Existe Anna Folgado Folgado?
+		
+		//¿Existe algún Lionel Messi?
+		
+		//¿Cuántos directores hay?
+	}
+
+}
